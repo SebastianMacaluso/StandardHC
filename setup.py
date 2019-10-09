003f7c40a663f1d06aadcad462244b0d03309af7
Sebastian Macaluso <sm4511@nyu.edu>
2019-10-09 17:13:07 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="ReclusterTreeAlgorithms",
+    version="0.1",
+    description="Reclustering Algorithms for Binary Trees",
+    url="https://github.com/SebastianMacaluso/ReclusterTreeAlgorithms",
+    author="Kyle Cranmer, Sebastian Macaluso, Duccio Pappadopulo",
+    author_email="sm4511@nyu.edu",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+)
