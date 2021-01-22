32ad3144c49be130e5c5566c3e62b0dee3d58ad1
Sebastian Macaluso <sm4511@nyu.edu>
2021-01-22 12:00:55 -0300

    New package structure

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 import setuptools
 
 setuptools.setup(
-    name="ReclusterTreeAlgorithms",
-    version="0.1",
-    description="Reclustering Algorithms for Binary Trees",
-    url="https://github.com/SebastianMacaluso/ReclusterTreeAlgorithms",
+    name="StandardHC",
+    version="0.0.1",
+    description="Hierarchical Clustering Algorithms for Jet Physics. Includes:"
+                "1) Likelihood based algorithms: Greedy and Beam Search"
+                "2) Standard heuristic algorithms for jet physics such as kt, anti-kt and CA",
+    url="https://github.com/SebastianMacaluso/StandardHC",
     author="Kyle Cranmer, Sebastian Macaluso, Duccio Pappadopulo",
     author_email="sm4511@nyu.edu",
     license="MIT",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    # packages=setuptools.find_packages(),
     zip_safe=False,
 )
